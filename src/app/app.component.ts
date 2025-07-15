2f83568a989173ff66ac4715141b23840b856d98
Alana Maiumy Gaspar <alanamaiumy@alunos.utfpr.edu.br>
2025-07-15 17:51:20 -0300

    Atualizações de Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,43 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   technologies = [
-    { name: 'Angular', iconClass: 'devicon-angularjs-plain' },
-    { name: 'TypeScript', iconClass: 'devicon-typescript-plain' },
-    { name: 'PostgreSQL', iconClass: 'devicon-postgresql-plain' },
-    { name: 'Vue.js', iconClass: 'devicon-vuejs-plain' },
-    { name: 'Laravel', iconClass: 'devicon-laravel-plain' },
-    { name: 'PHP', iconClass: 'devicon-php-plain' },
-    { name: 'JavaScript', iconClass: 'devicon-javascript-plain' }
+    { name: 'Angular', iconClass: 'devicon-angularjs-plain', color: '#e57373' },
+    { name: 'TypeScript', iconClass: 'devicon-typescript-plain', color: '#64b5f6' },
+    { name: 'PostgreSQL', iconClass: 'devicon-postgresql-plain', color: '#7986cb' },
+    { name: 'Vue.js', iconClass: 'devicon-vuejs-plain', color: '#81c784' },
+    { name: 'Laravel', iconClass: 'devicon-laravel-plain', color: '#f06292' },
+    { name: 'PHP', iconClass: 'devicon-php-plain', color: '#b39ddb' },
+    { name: 'JavaScript', iconClass: 'devicon-javascript-plain', color: '#fff176' }
   ];
 
   projects = [
-    { title: 'teste', description: 'testetesteteste' },
+     {
+      title: 'teste',
+      description: 'teste',
+      previewImage: 'teste',
+      siteUrl: 'teste',
+      githubUrl: 'teste'
+    },
     { title: 'teste', description: 'testetesteteste' },
     { title: 'teste', description: 'testetesteteste' },
     { title: 'teste', description: 'testetesteteste' },
     { title: 'teste', description: 'testetesteteste' },
     { title: 'teste', description: 'testetesteteste' }
   ];
+
+  selectedProject: any = null;
+
+  openModal(project: any) {
+    this.selectedProject = project;
+  }
+
+  closeModal() {
+    this.selectedProject = null;
+  }
+
+  goToLinks() {
+  if (!this.selectedProject) return;
+  window.open(this.selectedProject.siteUrl, '_blank');
+  window.open(this.selectedProject.githubUrl, '_blank');
+}
 }
